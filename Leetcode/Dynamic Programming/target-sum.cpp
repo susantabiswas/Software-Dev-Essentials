68e0639a97aac35c1433ced3ad47a9dd7c898fe1
Susanta Biswas <susantasky@gmail.com>
2020-01-01 00:16:40 +0530

    Create target-sum.cpp

---
+++ Leetcode/Dynamic Programming/target-sum.cpp
@@ -0,0 +1,43 @@
+/*
+    https://leetcode.com/problems/target-sum/
+	
+	The given sets can be considered as P and N. where N contains the numbers of other set which are
+	subtracted.
+	P + N = Target(T)
+	P + N + P + N = T + P + N
+	2P = T + (P + N)
+	2P = T + S
+	P = (T + S) / 2
+	
+	=> (T + S) should be even and P = (T + S)/2
+  
+  Ref:
+  https://leetcode.com/problems/target-sum/discuss/97334/Java-(15-ms)-C%2B%2B-(3-ms)-O(ns)-iterative-DP-solution-using-subset-sum-with-explanation
+*/
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        // the sum of S and negative set should be even
+        if(S > total || (S + total) % 2 != 0)
+            return 0;
+        
+        int positive_set_sum = (S + total) / 2;
+        
+        // each entry i: num of ways in which sum i is possible using the given arrays elements 
+        vector<int> dp(positive_set_sum + 1, 0);
+        // sum zero is always possible
+        dp[0] = 1;
+        
+        for(const int& num: nums) {
+            // starting from back to avoid addition of redundant terms with the 
+            // lower values when doing dp[i - num]
+            for(int i = positive_set_sum; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        
+        return dp[positive_set_sum];
+    }
+};

89112ae577d6d830aababae032316532cc762529
Susanta Biswas <susantasky@gmail.com>
2020-01-19 16:52:43 +0530

    Update find-the-duplicate-number.cpp

--- Leetcode/Binary_Search/find-the-duplicate-number.cpp
+++ Leetcode/Binary_Search/find-the-duplicate-number.cpp
@@ -3,6 +3,36 @@
 */
 class Solution {
 public:
+    // Using Floyd Cycle detection
+    // TC: O(N), SC: O(1)
+    
+    /*
+        Assume each nums value as an address just like in linked list node addr.
+        Then since there is one number whith duplicates, that means there are multiple instances
+        of the same address, so it is a cycle just like in linked list. Do the same thing
+        as in linked list.
+    */
+    int floydSolution(vector<int>& nums) {
+        // Each index is taken 1 based, as if it is zero based, then
+        // for nums[2] = 3, if we goto nums[nums[2] - 1], it is again nums[2]
+        // infinite loop
+        int hare = nums[0], tortoise = nums[0];
+        
+        do {
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+        } while(hare != tortoise);
+        
+        // to find the starting of cycle, make tortoise to start from begining
+        tortoise = nums[0];
+        while(hare != tortoise) {
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        }
+        
+        return hare;
+    }
+    
     // Using Binary search
     // TC: O(nlogn), SC: O(1)
     int binarySearchSolution(vector<int>& nums) {
@@ -48,6 +78,7 @@ public:
     
     int findDuplicate(vector<int>& nums) {
         // return sortingSolution(nums);
-        return binarySearchSolution(nums);
+        // return binarySearchSolution(nums);
+        return floydSolution(nums);
     }
 };

7cdea8bd9c76380795fa03a8ba5b2e0ac65b75fa
Susanta Biswas <susantasky@gmail.com>
2020-01-20 20:59:15 +0530

    Update implement-trie-prefix-tree.cpp

--- Leetcode/Trie/implement-trie-prefix-tree.cpp
+++ Leetcode/Trie/implement-trie-prefix-tree.cpp
@@ -5,28 +5,28 @@ class Trie {
 private:
     struct TrieNode {
         // for keeping track of character child nodes
-        unordered_map<char, TrieNode*> leaves;
+        unordered_map<char, unique_ptr<TrieNode>> leaves;
         // marks end of string
         bool is_string = false;
     };  
-    TrieNode* root_ = nullptr;
+    unique_ptr<TrieNode> root_ = nullptr;
 public:
     
     /** Initialize your data structure here. */
     Trie() {
         // create the root
-        root_ = new TrieNode();
+        root_ = make_unique<TrieNode>(TrieNode());
     }
     
     /** Inserts a word into the trie. */
     void insert(string word) {
-        TrieNode* root = root_;
+        TrieNode* root = root_.get();
         for(const char c: word) {
             // if current char is not there as a leaf, create
             if(root->leaves.find(c) == root->leaves.end())
-                root->leaves[c] = new TrieNode();
+                root->leaves[c] = make_unique<TrieNode>(TrieNode());
             // go to next node
-            root = root->leaves[c];
+            root = root->leaves[c].get();
         }
         // mark the end of current string
         root->is_string = true;
@@ -34,22 +34,24 @@ public:
     
     /** Returns if the word is in the trie. */
     bool search(string word) {
-        TrieNode* curr = root_;
+        TrieNode* curr = root_.get();
+        // if at any char, we don't find a node for that, means not found
         for(const char& c: word) {
             if(curr->leaves.find(c) == curr->leaves.end())
                 return false;
-            curr = curr->leaves[c];
+            curr = curr->leaves[c].get();
         }
+        // check if string ending here is a a complete word or not
         return curr->is_string;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     bool startsWith(string prefix) {
-        TrieNode* curr = root_;
+        TrieNode* curr = root_.get();
         for(const char& c: prefix) {
             if(curr->leaves.find(c) == curr->leaves.end())
                 return false;
-            curr = curr->leaves[c];
+            curr = curr->leaves[c].get();
         }
         return true;
     }

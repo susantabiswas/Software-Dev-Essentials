8a1ce7a570f6c9f6d1109302058a4031dc40e84b
Susanta Biswas <susantasky@gmail.com>
2020-03-04 21:04:34 +0530

    Update partition-equal-subset-sum.cpp

--- Leetcode/Dynamic Programming/partition-equal-subset-sum.cpp
+++ Leetcode/Dynamic Programming/partition-equal-subset-sum.cpp
@@ -1,14 +1,20 @@
-/*
-    https://leetcode.com/problems/partition-equal-subset-sum/
-*/
 class Solution {
 public:
     // Solution using bitset
     // Basically we use bitset array and apply shift operation for all the 
-    // elements of array.
+    // elements of array. Initially only 0th pos is achievable so it is set, then 
+    // as we keep adding more numbers we shift the already added numbers by that amount.
+    /* Eg: ini: 00000001
+            num: 3, shift bits array by 3, which makes 0 + 3
+            00000001 << 3 ==> 00001000, then take the OR with prev ==> 00001001
+            num: 2, shift bits array by 2, which makes 0 + 2, 3 + 5, so both 2 and 5 will be marked
+            00101201
+    */
     // Since the sum in the two subsets needs to be same, that is basically saying 
     // the sum of each set should be half of total sum, we check in the end if the
     // half of total sum is possible
+    
+    // TC: O(N*b), b: no. of bits in the bitset, N: array length
     bool canPartitionBitset(vector<int>& nums) {
         // take total sum of all elements
         int total = accumulate(nums.begin(), nums.end(), 0);
@@ -17,10 +23,12 @@ public:
         if(total % 2 != 0)
             return false;
         
-        bitset<100 * 200 + 1> bits(1);
+        bitset<100 * 200 + 1> bits;
+        // zero sum can be achieved with null set
+        bits[0] = 1;
         for(const int& num : nums)
             bits = bits | bits << num;
-        
+        // check if total/2 is achievable or not
         return bits[total >> 1];
     }
     
@@ -33,31 +41,26 @@ public:
             return false;
         
         // each entry 'i': sum i can be formed by subsets of elements
-        vector<bool> dp(nums.size() + 1, false);
+        vector<bool> dp(total/2 + 1, false);
         // sum of zero is always possible
         dp[0] = true;
-        /*
-        constructing sum/2 to 1, constructing
-        this way can avoid addition of redundant terms in some problems
-        for(const int& num: nums) {
-            // we need to check if sum 'i' is possible 
-            for(int i = total / 2; i >= num; i--) {
-                dp[i] = dp[i] || dp[i - num];
-            }
-        }
-        */
-        // constructing dp from 1 to sum/2
-        for(int i = 1; i <= total / 2; i++) {
-            for(const int& num : nums) {
-                int req_val = i - num;
+        // Since this uses 1D DP state, it is imp to make sure that for amount i,
+        // we see the contributing numbers only once as it is like a 0-1 knapsack problem.
+        for(const int& num : nums) { // ensures for numbers are not repeated
+            for(int i = total / 2; i >= 1; i--) {   
+                // since end goal is to get total/2 dp state,
+                // so starting from end ensures that nums contributing to smaller number are not repeated
+                // for larger numbers, like num 3 used for sum 3, and also for sum = 6(3 + 3) 
+                int req_val = i - num;              
                 if(req_val >= 0)
                     dp[i] = dp[i] || dp[req_val];
             }
         }
-        return dp[nums.size()];
+        return dp[total/2];
     }
     
     bool canPartition(vector<int>& nums) {
-        return canPartitionBitset(nums);
+        return canPartitionDPTabular(nums);
+        //return canPartitionBitset(nums);
     }
 };

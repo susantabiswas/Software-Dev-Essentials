c201c55468e9ba9856b8b0db57fcfa1715196b97
Susanta Biswas <susantasky@gmail.com>
2020-05-05 07:33:08 +0530

    Update first-missing-positive.cpp

--- Leetcode/Arrays/first-missing-positive.cpp
+++ Leetcode/Arrays/first-missing-positive.cpp
@@ -11,12 +11,25 @@
     TC: O(N)
     SC: O(1)
 */
+/*
+    https://leetcode.com/problems/first-missing-positive/solution/
+    
+    The idea is to move each number to its correct 0-indexed position. Then traverse
+    the array for the second time and the first index where the number doesn't match is
+    the answer. If the array has everything in place, then N + 1 is the answer.
+    The fact that the array size is N, so even if there are numbers greater than N,
+    they need not be considered as the array that should be at that place can always be the
+    answer even if all other numbers are present, so need to consider > N.
+    
+    TC: O(N)
+    SC: O(1)
+*/
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         for(int i = 0; i < nums.size();) {
             // swap the current number to its correct position
-            if(nums[i] - 1 >= 0 && nums[i] - 1 < nums.size()) {
+            if(nums[i] >= 0 && nums[i] - 1 >= 0 && nums[i] - 1 < nums.size()) {
                 swap(nums[nums[i]-1], nums[i]);
             }
             // if the current number is -ve, 0 or greater than array length or

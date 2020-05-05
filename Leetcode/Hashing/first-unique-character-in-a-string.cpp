654a8211420d7bdb45866154d34187486f89a82f
Susanta Biswas <susantasky@gmail.com>
2020-05-05 14:30:17 +0530

    Create first-unique-character-in-a-string.cpp

---
+++ Leetcode/Hashing/first-unique-character-in-a-string.cpp
@@ -0,0 +1,34 @@
+/*
+    https://leetcode.com/problems/first-unique-character-in-a-string/
+    
+    TC: O(N), N: length of string
+    SC: O(M), M: unique chars
+*/
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int idx = INT_MAX;
+        unordered_map<char, int> pos;
+        
+        // find the position of each char in string,
+        // incase a char was already seen, we dont update
+        // its position value but make it negative to indicate
+        // that the char was already seen
+        for(int i = 0; i < s.size(); i++) {
+            // first seeing the char, save its 1-based index
+            if(pos.count(s[i]) == 0)
+                pos[s[i]] = i+1;
+            // char appearing again, so make it negative
+            else if(pos[s[i]] > 0)
+                pos[s[i]] *= -1;
+        }
+        
+        // find the smallest index of unique number
+        for(auto it = pos.begin(); it != pos.end(); it++) {
+            // if the char is unique, then its index will be +ve
+            if(it->second > 0)
+                idx = min(idx, it->second);
+        }
+        return idx == INT_MAX ? -1 : idx-1;
+    }
+};

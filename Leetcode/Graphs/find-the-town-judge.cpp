dfce701cf3dd4393005479ff0292689c08331619
Susanta Biswas <susantasky@gmail.com>
2020-05-10 22:42:01 +0530

    Update and rename find_the_town_judge.cpp to find-the-town-judge.cpp

--- Leetcode/Graphs/find-the-town-judge.cpp
+++ Leetcode/Graphs/find-the-town-judge.cpp
@@ -2,6 +2,36 @@
   https://leetcode.com/problems/find-the-town-judge/submissions/
 */
 
+// SOLUTION 1
+/*
+    https://leetcode.com/problems/find-the-town-judge/submissions/
+    
+    TC: O(N), SC: O(N)
+*/
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        // We can consider each label as a graph node
+        // find the indegree and out degree of each node
+        vector<int> indegree(N, 0), outdegree(N, 0);
+        for(auto trust_edge: trust) {
+            ++indegree[trust_edge[1] - 1];
+            ++outdegree[trust_edge[0] - 1];
+        }
+        
+        // now check which node has N-1 indegree and for that node
+        // check if its outdegree is also 0, if both cond. are true, then that
+        // ndoe is town judge
+        for(int i = 0; i < indegree.size(); i++) {
+            if(indegree[i] == N-1 && outdegree[i] == 0)
+                return i + 1;
+        }
+        return -1;
+    }
+};
+
+///////////////////////////////////
+// SOLUTION 2
 class Solution {
 public:
     

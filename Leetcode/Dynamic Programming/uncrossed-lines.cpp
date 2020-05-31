db47299764f64ca40321dd60fb7409ebc2cdf7b4
Susanta Biswas <susantasky@gmail.com>
2020-05-31 09:32:25 +0530

    Create uncrossed-lines.cpp

---
+++ Leetcode/Dynamic Programming/uncrossed-lines.cpp
@@ -0,0 +1,40 @@
+/*
+    https://leetcode.com/problems/uncrossed-lines/
+    
+    If we look at the problem carefully, it is actually finding the longest common subsequence.
+    Since we have to match the same numbers in both the arrays ensuring that we get the maximum
+    length and also make sure that the lines dont overlap, that is basically saying find a
+    subsequence in A that matches B. 
+*/
+class Solution {
+public:
+    
+    // TC: O(N^2), SC:O(MN)
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        const int M = A.size(), N = B.size();
+        vector<vector<int>> dp(M+1, vector<int>(N+1, 0));
+        
+        // when the first array is empty, no lines can connect
+        for(int i = 0; i <= N; i++)
+            dp[0][i] = 0;
+        
+        // when the second array is empty, no lines can connect
+        for(int i = 0; i <= M; i++)
+            dp[i][0] = 0;
+        
+        // Now the aim is to find the longest common subsequence 
+        for(int i = 1; i <= M; i++)
+            for(int j = 1; j <= N; j++) {
+                // if the chars are same
+                if(A[i-1] == B[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else {
+                    // when they are diff, try to find the max length with
+                    // i-1 and j-1 chars separately
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+                
+        return dp[M][N];
+    }
+};

3444a6e3e6fd96b51414f10e9c4a3dd015841987
Susanta Biswas <susantasky@gmail.com>
2020-06-06 21:45:25 +0530

    Create merge-k-sorted-lists.cpp

---
+++ Leetcode/Linked_List/merge-k-sorted-lists.cpp
@@ -0,0 +1,82 @@
+/*
+    https://leetcode.com/problems/merge-k-sorted-lists/submissions/
+    
+    The idea is to merge the lists in tournament tree order to mnimize the 
+    number of merge operations.
+    
+    TC: O(NKlogK), N: length of longest list
+        there will be logK merge operations and a worst case list of size NK will be merged
+        = 2N*K/2 + 4N*K/2 + ........1
+        = NK + NK ....... + 1
+        = logK * NK
+        = NK * logK
+    SC: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // Merges two lists into one
+    ListNode* mergeList(ListNode* first, ListNode* second) {
+        // dummy head to make handling head operations easier
+        ListNode *dummy_head = new ListNode(-1), *tail = dummy_head;
+        
+        while(first && second) {
+            if(first->val < second->val) {
+                tail->next = first;
+                first = first->next;
+            }
+            else {
+                tail->next = second;
+                second = second->next;
+            }
+            
+            // add the node to final list
+            tail = tail->next;
+        }
+        
+        // add any remaining nodes
+        ListNode *t = first ? first : second;
+        while(t) {
+            tail->next = t;
+            tail = tail->next;
+            t = t->next;
+        }
+        
+        ListNode *head = dummy_head->next;
+        delete dummy_head;
+        
+        return head;
+    }
+    
+    
+    // Merges the lists recursively in a tournament tree manner
+    ListNode* mergeLists(int start, int end, vector<ListNode*>& lists) {
+        // base case
+        if(start == end) {
+            return lists[start];
+        }
+        if(start < end) {
+            int mid = start + (end - start) / 2;
+            ListNode *left = mergeLists(start, mid, lists);
+            ListNode *right = mergeLists(mid + 1, end, lists);
+            ListNode *merged_head = mergeList(left, right);
+            return merged_head;
+        }
+        return nullptr;
+    }
+    
+    // Driver 
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return mergeLists(0, lists.size() - 1, lists);
+    }
+};

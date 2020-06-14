a17516aec3832e500815540fbe89741ea40d00fd
Susanta Biswas <susantasky@gmail.com>
2020-06-14 10:49:18 +0530

    Create peak-index-in-a-mountain-array.cpp

---
+++ Leetcode/Binary_Search/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,28 @@
+/*
+    https://leetcode.com/problems/peak-index-in-a-mountain-array/submissions/
+    
+    Idea is to skip all the elements lesser than peak and making sure that one 
+    of the limits always points to the greater element around mid.
+    
+    Here we use right index to make sure that it always points to the greatest.
+    
+*/
+class Solution {
+public:
+    // TC: O(logN)
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0, right = arr.size()-1;
+        
+        while(left + 1 <= right) {
+            int mid = left + (right - left) / 2;
+            // If we are on left side of peak
+            if(arr[mid] < arr[mid+1])
+                left = mid + 1;
+            // We are on right side of peak
+            else
+                right = mid;
+        }
+        
+        return right;
+    }
+};

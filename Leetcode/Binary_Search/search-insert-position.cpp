5f11729b439c569361038c2ad63d3b76f131a20f
Susanta Biswas <susantasky@gmail.com>
2020-06-17 15:57:50 +0530

    Create search-insert-position.cpp

---
+++ Leetcode/Binary_Search/search-insert-position.cpp
@@ -0,0 +1,21 @@
+/*
+    https://leetcode.com/problems/search-insert-position/submissions/ 
+    Simple Binary search
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+    }
+};

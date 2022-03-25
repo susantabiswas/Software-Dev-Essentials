079502ee55184deabf5eb8776a1e68dba1c27117
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-03-25 22:24:47 +0530

    removed set

--- Leetcode/Backtracking/subsets.cpp
+++ Leetcode/Backtracking/subsets.cpp
@@ -17,29 +17,24 @@ public:
     // Using backtracking
     // TC: O(N * 2^N), SC:O(2^N)
     void generateSubsets(vector<int>& nums, vector<int>& partial,
-                    int i, set<vector<int> >& s, vector<vector<int> >& result) {
+                    int i, vector<vector<int> >& result) {
         if(i == nums.size()) {
-            if(s.find(partial) == s.end()) {
-                s.emplace(partial);
-                result.emplace_back(partial);
-                return;
-            }
+            result.emplace_back(partial);
+            return;
         }    
         
         // for the current index, we can either include it or choose not to
         partial.emplace_back(nums[i]);
-        generateSubsets(nums, partial, i+1, s, result);
+        generateSubsets(nums, partial, i+1, result);
         partial.pop_back();
-        generateSubsets(nums, partial, i+1, s, result);
+        generateSubsets(nums, partial, i+1, result);
     }
     
     // driver function for backtracking
     vector<vector<int>> subsetsBacktracking(vector<int>& nums) {
-        // set for unique subsets
-        set<vector<int> > s;
         vector<int> partial;
         vector<vector<int> > result;
-        generateSubsets(nums, partial, 0, s, result);
+        generateSubsets(nums, partial, 0, result);
         
         return result;
     }
@@ -61,7 +56,7 @@ public:
     
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        //return subsetsBacktracking(nums);
-        return generateSubsetsIterative(nums);
+        return subsetsBacktracking(nums);
+        //return generateSubsetsIterative(nums);
     }
 };

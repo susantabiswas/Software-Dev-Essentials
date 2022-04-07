451ee3993485a28dd1827643128d7910702d0dbd
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-04-07 16:32:15 +0530

    added explanation

--- Leetcode/Dynamic Programming/divisor-game.cpp
+++ Leetcode/Dynamic Programming/divisor-game.cpp
@@ -7,6 +7,11 @@
     in the end we they will end up with a smaller prime number. So when the situation is
     like where only 1 can be used and Alice gets an even number then she can win else she losses.
     
+    Whoever gets 1 loses. If N is even, Alice picks 1 and the other player gets an odd number.
+    No matter even/odd is picked by the other playerm, the num becomes even => going back to prev
+    case. If N is odd, Alice picks something, the opponent gets even and he/she can follow the 
+    above strategy to make Alice lose.
+    The game goes on till someones gets and then they lose.
 */
 
 class Solution {

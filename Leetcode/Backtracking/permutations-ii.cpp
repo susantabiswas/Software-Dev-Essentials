f3861454e3a35a668bf05c0ec2bc345f955b287d
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-04-17 20:48:53 +0530

    updated TC

--- Leetcode/Backtracking/permutations-ii.cpp
+++ Leetcode/Backtracking/permutations-ii.cpp
@@ -5,7 +5,9 @@
     we need to swap each of the numbers with ith number once , add it to the current pmt,
     repeat the same for the remaining numbers.
     
-    TC: O(n!)
+    TC: O(n * n!), we are doing n * (n-1) * (n-2) .. (1) recursive calls => O(n!)
+    then at each call(n), we run the for loop n times and at leaf nodes, again n times
+    T(n) = n * T(n-1) 
     SC: (n! * n)
 */
 class Solution {

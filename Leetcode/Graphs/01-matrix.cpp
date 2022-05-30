ba9c6ff8cdd5f24af3fd61f69dfc05023112da3e
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-05-30 20:25:22 +0530

    Update 01-matrix.cpp

--- Leetcode/Graphs/01-matrix.cpp
+++ Leetcode/Graphs/01-matrix.cpp
@@ -1,4 +1,115 @@
 /*
+    https://leetcode.com/problems/01-matrix/
+    
+    
+    The problem can be thought of as shortest path problem.
+    For any of the nodes with 1, the source can be any of the 0 nodes.
+    We can use either BFS or Djikstra to find the shortest distance to each node
+    from the source nodes.
+    
+    Solution 1: Djikstra
+    TC: O(MNlog(MN))
+    SC: O(MN)
+    
+    Solution 2: BFS
+    TC: O(MN)
+    SC: O(MN)
+*/
+class Solution {
+public:
+    vector<vector<int>> djikstraSol(vector<vector<int>>& mat) {
+        int M = mat.size(), N = mat[0].size();
+        vector<vector<int>> distance(M, vector<int>(N, -1));
+        
+        auto comp = [&](const array<int, 3>& a, const array<int, 3>& b) { return a[0] > b[0]; };
+        priority_queue<array<int, 3>, vector<array<int, 3> >, decltype(comp)> min_heap(comp);
+        
+        // Add the source nodes with 0
+        for(int i = 0; i < M; i++)
+            for(int j = 0; j < N; j++)
+                if(mat[i][j] == 0)
+                    min_heap.push({0, i, j});
+        
+        vector<vector<int>> directions = {{1, 0}, {-1, 0},
+                                          {0, 1}, {0, -1}};
+        
+        while(!min_heap.empty()) {
+            auto [dist, row, col] = min_heap.top();
+            min_heap.pop();
+            
+            // already processed, so skip 
+            if(distance[row][col] != -1)
+                continue;
+            
+            // set the dist
+            distance[row][col] = dist;
+            
+            // visit the unvisited neighbor
+            for(auto direction: directions) {
+                int r = row + direction[0], c = col + direction[1];
+                
+                // check for out of bound and then if that neighbor is  
+                // unvisited node with 1
+                if(r >= 0 && r < M && c >= 0 && c < N
+                  && distance[r][c] == -1 && mat[r][c] == 1) {
+                    min_heap.push({dist + 1, r, c});
+                } 
+            }
+        }
+        
+        return distance;
+    }
+    
+    // Sol 2: BFS Solution
+    // TC: O(MN)
+    // SC: O(MN)
+    vector<vector<int>> bfsSol(vector<vector<int>>& mat) {
+        int M = mat.size(), N = mat[0].size();
+        vector<vector<int>> distance(M, vector<int>(N, -1));
+        
+        queue<array<int, 3>> q;
+        
+        // Add the source nodes with 0
+        for(int i = 0; i < M; i++)
+            for(int j = 0; j < N; j++)
+                if(mat[i][j] == 0) {
+                    q.push({0, i, j});
+                    distance[i][j] = 0;
+                }
+        
+        vector<vector<int>> directions = {{1, 0}, {-1, 0},
+                                          {0, 1}, {0, -1}};
+        
+        while(!q.empty()) {
+            auto [dist, row, col] = q.front();
+            q.pop();
+            
+            // visit the unvisited neighbor
+            for(auto direction: directions) {
+                int r = row + direction[0], c = col + direction[1];
+                
+                // check for out of bound and then if that neighbor is  
+                // unvisited node with 1
+                if(r >= 0 && r < M && c >= 0 && c < N
+                  && distance[r][c] == -1 && mat[r][c] == 1) {
+                    q.push({dist + 1, r, c});
+                    // set the dist
+                    distance[r][c] = dist + 1;
+                } 
+            }
+        }
+        
+        return distance;
+    }
+    
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        // return djikstraSol(mat);
+        return bfsSol(mat);
+    }
+};
+
+
+/*
     https://leetcode.com/problems/01-matrix/submissions/
     
     We do BFS for finding the shortest distance. We make the initial zeros

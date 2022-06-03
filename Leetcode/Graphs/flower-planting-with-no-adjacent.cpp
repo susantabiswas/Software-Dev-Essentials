f86ff32843fea4c23c8ee22a906cd85230ae46e2
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-06-03 17:33:21 +0530

    Update flower-planting-with-no-adjacent.cpp

--- Leetcode/Graphs/flower-planting-with-no-adjacent.cpp
+++ Leetcode/Graphs/flower-planting-with-no-adjacent.cpp
@@ -1,13 +1,59 @@
 /*
     https://leetcode.com/problems/flower-planting-with-no-adjacent/submissions/
     
+    Solution :Greedy
     Since none of the nodes have more than 3 neighbors, we can select the 
     color not used by the neighbors.
 
-    TC: O(3*N) ~ O(N)    
+    TC: O(3*N) ~ O(N)
+    
+    Solution: BFS
+    TC: O(N)
 */
 class Solution {
 public:
+    // Solution : BFS
+    // TC: O(n)
+    // SC: O(n)
+    // Idea is to assign the 1st node of the component with color 1 and from there 
+    // assign the next nodes the next lexographical color. For a given node, its neighbor
+    // might be already colored, in that scenario, assign the next color of current node to it.
+    vector<int> bfs(int n, vector<vector<int>>& graph) {
+        // node_color[i] = color of ith node
+        vector<int> node_color(n, -1);
+        
+        for(int node = 0; node < n; node++) {
+            // unprocessed node and start of a new component
+            if(node_color[node] == -1) {
+                queue<int> q;
+                q.push(node);
+                // assign 1 to the initial node
+                node_color[node] = 1;
+                
+                while(!q.empty()) {
+                    auto curr = q.front();
+                    q.pop();
+
+                    for(auto neighbor: graph[curr]) {
+                        // if neighbor is uncolored, give the next lexographical color
+                        if(node_color[neighbor] == -1) {
+                            // % operator will give 0 as the next color of 4, so max of 1 ensures a valid color
+                            node_color[neighbor] = max(1, (node_color[curr] + 1) % 5);
+                            q.push(neighbor);
+                        }
+                        // if the neighbor already has the same color, assign it the next color
+                        else if(node_color[neighbor] == node_color[curr]) {
+                            node_color[neighbor] = max(1, (node_color[curr] + 1) % 5);
+                        }
+                    }
+                }
+            }
+        }
+        return node_color;
+    }
+    
+    // Solution: Greedy
+    // TC: O(N)
     // Graph coloring using greedy approach
     vector<int> colorGraph(vector<vector<int>> g) {
         // -1 indicates uncolored
@@ -38,6 +84,7 @@ public:
             g[edge[1] - 1].emplace_back(edge[0] - 1);
         }
         
-        return colorGraph(g);
+        // return colorGraph(g);
+        return bfs(n, g);
     }
 };

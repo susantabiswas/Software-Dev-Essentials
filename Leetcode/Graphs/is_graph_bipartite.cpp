ff197c2b5476d16ffef37f801246453cbaa5602f
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-06-04 17:36:33 +0530

    Update is_graph_bipartite.cpp

--- Leetcode/Graphs/is_graph_bipartite.cpp
+++ Leetcode/Graphs/is_graph_bipartite.cpp
@@ -1,46 +1,83 @@
 /*
-  Is graph bipartite.
-  https://leetcode.com/problems/is-graph-bipartite/
+    https://leetcode.com/problems/is-graph-bipartite/
+    
+    Core idea is to check whether we can color the graph nodes with just 2 colors
+    such that the adjacent vertices don't get the same color. If we can do that, then 
+    graph is bipartite.
+    
+    We can color the graph either using BFS or DFS.
 */
-
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
+    bool dfsColoring(int curr, vector<int>& color, 
+                     vector<vector<int>>& graph) {
         
-        // try all vertices incase the graph is not connected
-        for(int j = 0; j < graph.size(); j++)
-        {        
-            vector<vector<int> > color(graph.size(), vector<int>(1, -1));
-            queue<int> q;
-
-            // add the first vertex
-            q.emplace(j);
-            // give the vertex a color, we give it 0
-            color[j][0] = 0;
-
-            while(!q.empty()) {
-                int curr = q.front();
-                q.pop();
-
-                // color its adjacent vertices
-                for(int i = 0; i < graph[curr].size(); i++) {
-                    // if any of its adjacent vertices have the same color,
-                    // then it is not bipartite
-
-                    // if the adjacent vertex is not colored
-                    if(color[graph[curr][i]][0] == -1) {
-                        // give it a different color 
-                        color[graph[curr][i]][0] = color[curr][0] == 0 ? 1:0;
-                        // add to queue
-                        q.emplace(graph[curr][i]);
-                    }
-                    else {
-                        if(color[graph[curr][i]][0] == color[curr][0])
+        for(auto neighbor: graph[curr]) {
+            // unvisited neighbor, assign the other color
+            if(color[neighbor] == -1) {
+                color[neighbor] = !color[curr];
+                if(!dfsColoring(neighbor, color, graph))
+                    return false;
+            }
+            // visited node, if it has the same color, can't be bipartite
+            else if(color[neighbor] == color[curr])
+                return false;
+        }   
+        return true;
+    }
+    
+    // Solution DFS
+    // TC: O(N)
+    // SC: O(N)
+    bool dfsSol(vector<vector<int>>& graph) {
+        // Two color values: 0 and 1
+        vector<int> color(graph.size(), -1);
+        
+        // Source vertex colored with 1
+        for(int node = 0; node < graph.size(); node++) {
+            if(color[node] == -1) {
+                color[node] = 1;
+                if(!dfsColoring(node, color, graph))
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    // Solution BFS
+    // TC: O(N)
+    // SC: O(N)
+    bool bfsSol(vector<vector<int>>& graph) {
+        vector<int> color(graph.size(), -1);
+        
+        for(int node = 0; node < graph.size(); node++) {
+            if(color[node] == -1) {
+                queue<int> q;
+                q.push(node);
+                // give it color 1
+                color[node] = 1;
+                
+                while(!q.empty()) {
+                    auto curr = q.front();
+                    q.pop();
+                    
+                    for(auto neighbor: graph[curr]) {
+                        if(color[neighbor] == -1) {
+                            color[neighbor] = !color[curr];
+                            q.push(neighbor);
+                        }
+                        else if(color[neighbor] == color[curr])
                             return false;
                     }
                 }
             }
         }
         return true;
+    }
+    
+    bool isBipartite(vector<vector<int>>& graph) {
+        return dfsSol(graph);
+        // return bfsSol(graph);
     }
 };

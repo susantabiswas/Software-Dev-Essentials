545237d05e66efa6a6435d1204260a60bd7a0855
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-07-15 16:34:06 +0530

    Update longest-substring-with-at-most-k-distinct-characters.cpp

--- Leetcode/Sliding_Window/longest-substring-with-at-most-k-distinct-characters.cpp
+++ Leetcode/Sliding_Window/longest-substring-with-at-most-k-distinct-characters.cpp
@@ -8,6 +8,28 @@
 */
 class Solution {
 public:
+    /////////////////// SOLUTION 1
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int max_len = 0, n = s.size();
+        int start = 0, end = 0;
+        unordered_map<char, int> freq;
+        
+        while(end < n) {
+            // expand the window
+            ++freq[s[end++]];
+            // If the state is unsafe, make it safe again by shrinking the window
+            while(freq.size() > k) {
+                char ch = s[start++];
+                if(--freq[ch] == 0) 
+                    freq.erase(ch);
+            }
+            // update the maximum length
+            max_len = max(max_len, end - start);
+        }
+        return max_len;
+    }
+    
+    ////////////////// SOLUTION 2
     int lengthOfLongestSubstringKDistinct(string s, int k) {
         int i = 0, j = 0;
         int max_len = 0;

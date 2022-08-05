f5b1ba6dfdf077827372930ba687134604cb7019
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-08-05 16:37:51 +0530

    updated TC

--- Leetcode/Backtracking/n-queens.cpp
+++ Leetcode/Backtracking/n-queens.cpp
@@ -5,6 +5,33 @@
     TC: t(n) = nT(n-1) + O(1)
         O(n^n), n calls made at each stage and max depth of n levels, validating correcting pos takes O(1)
     SC: O(n), max stack depth + 3 vectors of multiple * n space
+
+    
+
+    Time complexity: O(N!)
+
+    Unlike the brute force approach, we will only place queens on squares that aren't 
+    under attack. For the first queen, we have NNN options. For the next queen, we won't 
+    attempt to place it in the same column as the first queen, and there must be at least 
+    one square attacked diagonally by the first queen as well. Thus, the maximum number of 
+    squares we can consider for the second queen is N−2. For the third queen, 
+    we won't attempt to place it in 2 columns already occupied by the first 2 queens, 
+    and there must be at least two squares attacked diagonally from the first 2 queens. 
+    Thus, the maximum number of squares we can consider for the third queen is N−4. 
+    This pattern continues, resulting in an approximate time complexity of N!.
+
+    While it costs O(N^2) to build each valid solution, the amount of valid 
+    solutions S(N) does not grow nearly as fast as N!, 
+    so O(N!)O(N! + S(N) * N^2) = O(N!)
+
+    Space complexity: O(N^2)
+
+    Extra memory used includes the 3 sets used to store board state, as well 
+    as the recursion call stack. All of this scales linearly with the number 
+    of queens. However, to keep the board state costs O(N^2), since 
+    the board is of size N * N. Space used for the output does not count towards 
+    space complexity.
+
 */
 class Solution {
 public:

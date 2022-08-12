00cff0e682bba139af0012c38cd524a48c9613d4
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-08-12 18:28:18 +0530

    fixed bug

--- Leetcode/Stack/maximum-score-of-a-good-subarray.cpp
+++ Leetcode/Stack/maximum-score-of-a-good-subarray.cpp
@@ -22,7 +22,7 @@ public:
                 s.pop();
                 
                 // This is the number before the lower bound
-                int lower_prev = s.empty() ? 0 : s.top();
+                int lower_prev = s.empty() ? -1 : s.top();
                 // Width of rectangle formed
                 int width = i - (s.empty() ? 0 : s.top() + 1);
                 int area = heights[curr] * width;
@@ -38,7 +38,7 @@ public:
             int curr = s.top();
             s.pop();
             
-            int lower_prev = s.empty() ? 0 : s.top();
+            int lower_prev = s.empty() ? -1 : s.top();
             int width = n - (s.empty() ? 0 : s.top() + 1);
             int area = heights[curr] * width;
             // Since upper bound is n for all remaining elements and k < n, we don't check for upper limit
@@ -51,47 +51,5 @@ public:
     
     int maximumScore(vector<int>& nums, int k) {
         return largestHistogram(nums, k);
-    }
-};
-////////////////////////////
-
-
-/*
-    https://leetcode.com/problems/maximum-score-of-a-good-subarray/submissions/
-    
-    Using sliding window we can cover the valid windows and find the max score.
-    Since i <= k <= j, so the subarray will always have index k, so we can expand 
-    starting from index k in both directions, each time going in the direction of
-    greater element.
-    
-    TC: O(N)
-    SC: O(1)
-*/
-
-class Solution {
-public:
-    int maximumScore(vector<int>& nums, int k) {
-        // starting point of window
-        int i = k, j = k;
-        // starting point: index k
-        int max_score = nums[k], min_num = nums[k], curr_min = nums[k];
-        
-        while(i > 0 || j < (int)nums.size() - 1) {
-            // decide whether to expand on left or right
-            if(i == 0)
-                ++j;
-            else if(j == nums.size()-1)
-                --i;
-            // both the indices can expand safely 
-            else if(nums[i-1] > nums[j+1])
-                --i;
-            else
-                ++j;
-            curr_min = min(nums[i], nums[j]);
-            min_num = min(min_num, curr_min);
-            max_score = max(max_score, min_num * (j - i + 1));
-            
-        }
-        return max_score;
     }
 };

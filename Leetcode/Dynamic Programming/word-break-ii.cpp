db8c44d680c9c86beb7a79e5497eddf7455c485e
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-08-14 18:45:24 +0530

    Update word-break-ii.cpp

--- Leetcode/Dynamic Programming/word-break-ii.cpp
+++ Leetcode/Dynamic Programming/word-break-ii.cpp
@@ -1,48 +1,51 @@
 /*
     https://leetcode.com/problems/word-break-ii/
     
-    TC: O(N^2) [Because of memoization]
+    TC: O(n * 2^N) [Because of memoization]
+    In total 2^N sentences can be there, constructing them takes O(n)
+    SC: O(n * 2^N)
+    
+    There can be 2^N no. of valid sentences, each having n chars.
 */
 class Solution {
 public:
-    bool findSentences(string& s, int start, string partial, 
-                       unordered_set<string>& words, unordered_set<string>& result,
-                      vector<bool>& dp) {
+    vector<string> findSentences(string& s, int start, unordered_set<string>& words,
+                                 unordered_map<int, vector<string>>& dp) {
+        // Result cached, return the sentences that can be formed from start position
+        if(dp.count(start))
+            return dp[start];
         
-        // all the chars covered
-        if(start == s.size()) {
-            result.emplace(partial);
-            return true;
-        }
-        // no. of sentences formed before segmentation from current position was done
-        int orig_sentences = result.size();
+        string substring;
         for(int i = start; i < s.size(); i++) {
-            // continue DFS only if valid segmentation is possible from the next char
-            if(dp[i+1] == true) {
-                string curr_word = s.substr(start, i - start + 1);
-                // word found, fix the current word and find the next word
-                if(words.find(curr_word) != words.end()) {
-                    findSentences(s, i + 1, 
-                                  partial + (partial.empty() ? curr_word : " " + curr_word),
-                                  words, result, dp);
+            // Instead of calling substring multiple times, we build the substring incrementally
+            substring += s[i];
+            
+            if(words.count(substring)) {
+                // Base case: End of string reached
+                if(i == s.size() - 1)
+                    dp[start].emplace_back(substring);
+                else {
+                    vector<string> sentences_from_here = findSentences(s, i + 1,
+                                                                    words, dp);
+                    
+                    // Add the current valid word prefix to the result obtained
+                    // This will result in a sentence formed out of s[start:N-1]
+                    for(auto sentence: sentences_from_here)
+                        dp[start].emplace_back(substring + " " + sentence);
                 }
             }
         }
-        // if no sentence could be formed when segmentation started from current start,
-        // make the current position as false
-        dp[start] = orig_sentences != result.size();
+        
         return dp[start];
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         // make a set of words for faster lookup
         unordered_set<string> words(wordDict.begin(), wordDict.end());
-        // dp(i): indicates that s[i : N-1] can be segmented into valid words or not 
-        vector<bool> dp(s.size() + 1, true);
-        unordered_set<string> result;
-        string partial;
+        // dp(i): Sentence that can be formed from ith position i.e in 
+        // s[i : N-1] which can be segmented into valid words 
+        unordered_map<int, vector<string>> dp;
         
-        findSentences(s, 0, partial, words, result, dp);
-        return vector<string>{result.begin(), result.end()};
+        return findSentences(s, 0, words, dp);
     }
 };

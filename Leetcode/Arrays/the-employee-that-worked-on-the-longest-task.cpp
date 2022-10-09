aaed993e64f5e753e0e9765de76ff813bad91165
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2022-10-09 13:03:38 +0530

    Create the-employee-that-worked-on-the-longest-task.cpp

---
+++ Leetcode/Arrays/the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,27 @@
+/*
+    https://leetcode.com/problems/the-employee-that-worked-on-the-longest-task/
+    
+    TC: O(N)
+    SC: O(1)
+    
+    Just keep track of duration of task for each of the workers.
+*/
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        int longest = logs[0][1];
+        int id = logs[0][0];
+        int end = logs[0][1];
+        
+        for(int i = 1; i < logs.size(); i++) {
+            int duration = logs[i][1] - end;
+            
+            if(duration > longest || (duration == longest && logs[i][0] < id)) {
+                id = logs[i][0];
+                longest = duration;
+            } 
+            end = logs[i][1];
+        }
+        return id;
+    }
+};

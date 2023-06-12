dea11c1611b4fc2fd91a1b3fc30e540350a4ea81
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-06-12 22:49:23 +0530

    Update kth-smallest-element-in-a-sorted-matrix.cpp

--- Leetcode/Heap/kth-smallest-element-in-a-sorted-matrix.cpp
+++ Leetcode/Heap/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -57,3 +57,91 @@ public:
         return *get<0>(min_heap.top());
     }
 };
+
+
+//////////////////////////////// SOLUTION 2: Binary Search
+class Solution {
+public:
+    // SOLUTION: Binary Search
+    // TC: O((m+n)log(S)), 
+    // SC: O(1)
+    // m = no. of rows, n = no. of cols, S = Search space (MaxNumber (bottom right) - MinNumber (top left))
+    /*
+        The idea is to use binary search on the search space range. In this case
+        search space is MinNumber..MaxNumber of matrix i.e top left to bottom right.
+        
+        Then we find a candidate number and check how many elements in the matrix are <= candidate.
+        Since the candidate may or may not be in the matrix, we look for the smallest candidate for
+        which there are atleast <= k no. of elements <= candidate.
+        
+        Proof why the final candidate will always exist in the matrix
+        
+        When we generate a number between the lower and upper bound, it might not necessarily be present in the
+        matrix but can still satisfy the BSearch criteria. So we look for the next smaller candidate satisying the 
+        criteria and keeping looking the smallest till the next candidate breaks the criteria.
+        
+        When one of the candidates actually exists, then any number less than that won't satisfy the criteria
+        of count(candidate) >= k. This is because if X is the actual candidate that exists in the matrix, then all the 
+        numbers > x, were seeing the contribution of count till x and were getting count(candidate) = y (which is >= k).
+        But any number < x, won't get the count contribution of x anymore and hence the count(candidate) < k (since x is not counted)
+        
+        nums = [1, 2, 20, 30, 30, 70, 80, 100], k = 5, ans should be 30
+        
+        !!! NOTE: Showing some scenarios, the candidates are not generated as per binary search, just showing why a smaller candidate will exist
+        
+        Let'say we generate a mid candidate 70 (doesnt exist) and it satisfies the count condition
+        countLessThanEqual(70) = 6 which is >= k, so right = 70
+        
+        Next candidate is say 50 (doesnt exist), countLessThanEqual(50) = 6 which is >= k, right = 50
+        Next candidate is say 30 (exists), countLessThanEqual(30) = 5 which is >= k, right = 30
+        Next candidate is say 20 (exists), countLessThanEqual(20) = 3 which is < k (the actual answer 30 was excluded and hance made the count < k)
+        
+        
+        
+    */
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size(), n = matrix[0].size();
+        
+        // lower and upper bound for binary search
+        int l = matrix[0][0], r = matrix[m-1][n-1];
+        
+        // Kth smallest element will have k elements(incl. itself) <= than itself
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            
+            // Since the current candidate's count >= k,
+            // it is a likely ans. If there are duplicates in the matrix
+            // then that explains > k part or if it is the exact
+            // answer then == k, so we make the upper bound as this likley ans candidate
+            if(countLessThanEqual(matrix, mid) >= k)
+                r = mid;
+            else
+                // Since count < k, no chance this is the ans candidate, safely ignore
+                // mid
+                l = mid + 1;
+        }
+        return r;
+    }
+    
+    // The reason why we count the equal elements as well is because
+    // those also contributes to the kth count
+    // eg [1, 2, 2, 2], k = 3, ans = 2, unless we count the 1st 2, we cannot conclude
+    // that the 2nd 2 is the 3rd smallest number
+    // TC: O(row + col)
+    int countLessThanEqual(vector<vector<int>>& matrix, int mid) {
+        int col = matrix[0].size() - 1;
+        // Tracks the no. of elements <= k
+        int count = 0;
+        
+        for(int row = 0; row < matrix.size(); row++) {
+            while(col >= 0 && matrix[row][col] > mid)
+                --col;
+            
+            // now matrix[row][col] is <= k, so all the elements
+            // in the current row from [0..col] are <= k
+            count += col + 1;
+        }
+        
+        return count;    
+    }
+};

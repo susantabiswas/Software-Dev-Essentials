012821d0e1d683bb6dea7b688a8e0da364a8b9f6
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-06-21 20:32:42 +0530

    Update accounts-merge.cpp

--- Leetcode/Graphs/accounts-merge.cpp
+++ Leetcode/Graphs/accounts-merge.cpp
@@ -67,3 +67,116 @@ public:
         return result;
     }
 };
+
+
+//////////////////////////////// Union-Find
+// TC: O(n) (Graph creation) + O(nlogm) (Union) + O(mlogm) (Sorting the emails)
+// n = total emails, m = unique emails
+class Solution {
+private:
+    class UnionFind {
+    public:
+        vector<int> root, size;
+        
+        UnionFind(int n) {
+            root.resize(n), size.resize(n, 1);
+            for(int i = 0; i < n; i++)
+                root[i] = i;
+        }
+        
+        int getRoot(int idx) {
+            while(idx != root[idx]) {
+                root[idx] = root[root[idx]];
+                idx = root[idx];
+            }
+            return idx;
+        }
+        
+        bool Union(int a, int b) {
+            int rootA = getRoot(a);
+            int rootB = getRoot(b);
+            
+            if(rootA != rootB) {
+                if(size[rootA] > size[rootB]) {
+                    size[rootA] += size[rootB];
+                    root[rootB] = rootA;
+                }
+                else {
+                    size[rootB] += size[rootA];
+                    root[rootA] = rootB;
+                }
+                return true;
+            }
+            return false;
+        }
+    };
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        // create the graph
+        // save the email -> name mapping
+        unordered_map<string, string> emailToName;
+        unordered_map<string, vector<string>> g;
+        unordered_map<string, int> nodeMap;
+        int idx = 0;
+        
+        for(auto account: accounts) {
+            string name = account[0], start = account[1];
+            
+            if(!g.count(start)) {
+                emailToName[start] = name;
+                g[start] = {};
+                nodeMap[start] = idx++;
+            }
+            
+            for(int i = 1; i < account.size(); i++) {
+                string email = account[i];
+                
+                if(!g.count(email)) {
+                    emailToName[email] = name;
+                    g[email] = {};
+                    nodeMap[email] = idx++;
+                }    
+                g[start].push_back(email);
+                g[email].push_back(start);
+            }
+        }
+        
+        vector<vector<string>> result;
+        unordered_set<string> visited;
+        
+        UnionFind uf(idx);
+        
+        for(auto account: accounts) {
+            string name = account[0], start = account[1];
+            
+            if(!g.count(start)) {
+                emailToName[start] = name;
+                g[start] = {};
+                nodeMap[start] = idx++;
+            }
+            
+            for(int i = 1; i < account.size(); i++) {
+                string email = account[i];
+                
+                if(!g.count(email)) {
+                    emailToName[email] = name;
+                    g[email] = {};
+                    nodeMap[email] = idx++;
+                }    
+                uf.Union(nodeMap[start], nodeMap[email]);
+            }
+        }
+        
+        unordered_map<int, vector<string>> allEmails;
+        for(auto [email, _]: g)
+            allEmails[uf.getRoot(nodeMap[email])].push_back(email);
+
+        for(auto [i, emails]: allEmails) {
+            sort(emails.begin(), emails.end());
+            string name = emailToName[emails[0]];
+            emails.insert(emails.begin(), name);
+            result.push_back(emails);
+        }
+        return result;
+    }
+};

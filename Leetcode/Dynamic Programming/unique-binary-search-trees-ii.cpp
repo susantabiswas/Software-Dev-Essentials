ef407b1ab23e9e5d12b89a5602527f9501800b0e
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-08-05 13:23:28 +0530

    Update unique-binary-search-trees-ii.cpp

--- Leetcode/Dynamic Programming/unique-binary-search-trees-ii.cpp
+++ Leetcode/Dynamic Programming/unique-binary-search-trees-ii.cpp
@@ -1,7 +1,10 @@
 /*
     95. Unique Binary Search Trees II
     https://leetcode.com/problems/unique-binary-search-trees-ii/
-
+    **************** NOTE******************* 
+    O(n*Cn) only happens with memoization, otherwise with the below code
+    we end of doing backtracking and repeating a lot of computations. Refer the 2nd solution of DP
+    
     Time: O(C0+C1+...Cn), where Cn is the Catalan number
     SC: O(n * Cn), Cn: Total Cn no. of BSTs with n nodes
 
@@ -66,5 +69,53 @@ public:
             return vector<TreeNode*>{};
         return createTree(1, n);
         
+    }
+};
+
+/*
+    https://leetcode.com/problems/unique-binary-search-trees-ii/
+
+    TC: O(n * Catalan Number) = O(n * (4^n/n^1.5)) ~ O(4^n/n^0.5)
+    SC: O(4^n/n^0.5)
+    
+    There are C(n) unique trees. C(n) = Catalan number = 4^n/n^1.5.
+    Also each tree consists finally of n nodes and while building the tree, we
+    need to create each of those unique nodes which takes O(n).
+*/
+class Solution {
+public:
+    vector<TreeNode*> genTrees(int start, int end, vector<vector<vector<TreeNode*>>>& dp) {
+        // base case
+        if(start > end)
+            return vector<TreeNode*>{ nullptr };
+        
+        if(dp[start][end].empty()) {
+            vector<TreeNode*> trees;
+        
+            for(int curr = start; curr <= end; curr++) {
+                vector<TreeNode*> leftSubtrees = genTrees(start, curr - 1, dp);
+                vector<TreeNode*> rightSubtrees = genTrees(curr + 1, end, dp);
+
+                for(auto left: leftSubtrees)
+                    for(auto right: rightSubtrees) {
+                        // current root
+                        TreeNode* root = new TreeNode(curr);
+
+                        root->left = left;
+                        root->right = right;
+
+                        trees.emplace_back(root);
+                    }
+            }
+
+            dp[start][end] = trees;
+        }
+        return dp[start][end];
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {
+        // dp[start][end] = vector<TreeNode*>{ All possible unique trees with nodes from start..end }
+        vector<vector<vector<TreeNode*>>> dp(n + 1, vector(n + 1, vector<TreeNode*>(0))); dp;
+        return genTrees(1, n, dp);
     }
 };

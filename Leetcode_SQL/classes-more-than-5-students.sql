ef35631ab61b1eb81088a1feb30b6e05b3ac449d
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-10-27 16:51:43 +0530

    Update classes-more-than-5-students.sql

--- Leetcode_SQL/classes-more-than-5-students.sql
+++ Leetcode_SQL/classes-more-than-5-students.sql
@@ -4,3 +4,20 @@ SELECT class
 FROM courses
 GROUP BY class
 HAVING COUNT(DISTINCT student) >= 5; 
+
+
+####### SOLUTION 2
+# For the source, we use the group by output of Courses and then on that output we 
+# filter the classes with student  count < 5
+SELECT class
+FROM ( 
+    SELECT class, count(student) AS num
+    FROM Courses
+    GROUP BY class
+) AS classes
+WHERE classes.num >= 5;
+
+
+
+
+

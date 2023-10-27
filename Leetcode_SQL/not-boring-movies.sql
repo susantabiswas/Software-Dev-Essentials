3b79bd1c102a3bbffb7b7868ba6a03e21a4653b7
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-10-27 16:33:59 +0530

    Update not-boring-movies.sql

--- Leetcode_SQL/not-boring-movies.sql
+++ Leetcode_SQL/not-boring-movies.sql
@@ -4,3 +4,10 @@ SELECT *
 FROM Cinema
 WHERE id % 2 <> 0 AND description <> 'boring'
 ORDER BY rating DESC;
+
+
+//////////// SOLUTION 2
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1 and description != 'boring'
+ORDER BY rating DESC;

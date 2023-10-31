31ecd35db6ce775f840f2ab8223d510e4660e673
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-10-31 15:27:20 +0530

    Update managers-with-at-least-5-direct-reports.sql

--- Leetcode_SQL/Joins/managers-with-at-least-5-direct-reports.sql
+++ Leetcode_SQL/Joins/managers-with-at-least-5-direct-reports.sql
@@ -3,6 +3,8 @@
 # 1. Inner join to get the corresponding manager name for each of the employees
 # 2. Then group by managerId
 # 3. Finally project the name of the manager with reportees >= 5
+
+# SOLUTION 1
 SELECT name
 FROM (
     SELECT name, count(managerId) AS reportees
@@ -14,3 +16,26 @@ FROM (
     GROUP BY managerId
 ) e
 WHERE reportees >= 5;
+
+# SOLUTION 2
+SELECT name
+FROM (
+    SELECT e1.managerId, e2.name
+    FROM Employee as e1
+    JOIN Employee as e2 ON e1.managerId = e2.id
+) emp
+GROUP BY managerId
+HAVING count(managerId) >= 5;
+
+# SOLUTION 3
+# 1. GROUP the managers by id and project only with >= 5 reportees
+# 2. Only project employees whose employee_id matches ones of the ids projected from subquery
+
+SELECT name
+FROM Employee
+WHERE id IN (
+    SELECT managerId
+    FROM Employee
+    GROUP BY managerId
+    HAVING count(managerId) >= 5
+);

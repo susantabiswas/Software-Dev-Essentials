b3a4026d7cba12bfe3c50bcb3eabab52abde791c
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-12-18 18:56:51 +0530

    Update the-maze-ii.cpp

--- Leetcode/Graphs/the-maze-ii.cpp
+++ Leetcode/Graphs/the-maze-ii.cpp
@@ -71,3 +71,59 @@ public:
         return djikstra(maze, start, destination);
     }
 };
+
+//////////////////////////////////// BFS Solution
+class Solution {
+public:
+    // TC: O(MN * min(M, N))
+    // Each node can only be visited via a path once, so at max a node will be
+    // visited via 3 directions at max (there will be atleast 1 side with a wall which made the ball stationary).
+    // If an already visited path is visited again, it will only increase the distance from source and wont 
+    // be added to queue.
+    // SC: O(MN)
+    
+    int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        vector<array<int, 2>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        int m = maze.size(), n = maze[0].size();
+        
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        // <row, col>
+        queue<array<int, 2>> q;
+        
+        // add the start pt and set its dist=0
+        q.push({start[0], start[1]});
+        dist[start[0]][start[1]] = 0;
+        
+        // Explore all the edges that can give potential paths
+        // Unlike dijikstra, here the elements are not picked based on any cost
+        // metric, so imp to traverse till we can.
+        // Since the  
+        while(!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+            
+            // explore the neighbors
+            for(auto [dx, dy]: dirs) {
+                int r = row, r_next = row + dx;
+                int c = col, c_next = col + dy;
+                
+                // move till not hit by a wall
+                while(r_next >= 0 && r_next < m && c_next >= 0 && c_next < n && maze[r_next][c_next] != 1) {
+                    r = r_next;
+                    c = c_next;
+                    
+                    r_next += dx, c_next += dy;
+                }
+                
+                int d = abs(r - row) + abs(c - col) + dist[row][col];
+                
+                if((r != row || c != col) && dist[r][c] > d) {
+                    dist[r][c] = d;
+                    q.push({r, c});
+                }
+            }
+        }   
+        
+        return dist[destination[0]][destination[1]] == INT_MAX ? -1 : dist[destination[0]][destination[1]];
+    }
+};

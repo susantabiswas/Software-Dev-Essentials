62c57a543463b3c99c3c41175c8ab1f5e3801120
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2024-01-06 14:12:03 +0530

    Update combination-sum-iii.cpp

--- Leetcode/Backtracking/combination-sum-iii.cpp
+++ Leetcode/Backtracking/combination-sum-iii.cpp
@@ -1,15 +1,27 @@
 /*
     https://leetcode.com/problems/combination-sum-iii/
     
-    Time Complexity: O(k * 9! / (9−k)!)
+    Time Complexity: 
+    O(k * C(9, k))
+    O(k * 9! / (9−k)!k!)
     
-        In a worst scenario, we have to explore all potential combinations to the very end, i.e. the sum n is a large number (n > 9∗9). At the first step, we have 9 choices, while at the second step, we have 8 choices, so on and so forth.
-        The number of exploration we need to make in the worst case would be P(9,K) = 9!(9−K)!, assuming that K<=9K<=9. By the way, K cannot be greater than 9, otherwise we cannot have a combination whose digits are all unique.
+        In a worst scenario, we have to explore all potential combinations to the very end, i.e. the sum n is a large number (n > 9∗9). 
+        At the first step, we have 9 choices, while at the second step, we have 8 choices, so on and so forth.
+        C(9, k) = 9 choose k, which is ways of choosing k numbers from 9, where the order doesn matter. In the code we can only
+        explore one order and not all the orders for the same set of numbers. Eg [1,2] is explored only not [2,1] hence it is Cmb not Pmt.
+        
+        The number of exploration we need to make in the worst case would be C(9,K) = 9!(9−K)!k!, assuming that K<=9K<=9. 
+        By the way, K cannot be greater than 9, otherwise we cannot have a combination whose digits are all unique.
         Each exploration takes a constant time to process, except the last step where it takes O(K) time to make a copy of combination.
-        To sum up, the overall time complexity of the algorithm would be 9!(9−K)! * k
+        To sum up, the overall time complexity of the algorithm would be 9!(9−K)!k! * k
+
+        Reason it is not O(k * 9^k) is because, after each kth call we are not necessarily left with k-1 elements. Eg when n=7 is picked, we are left with
+        only [8,9] for the next call, so saying T(k) = 9*T(k-1) is misleading as there will be 9 calls only in the 
+        1st time recursion starts.
+        O(k * 9^k) > C(9, k) 
     
     Space Complexity: O(K)
-        During the backtracking, we used a list to keep the current combination, which holds up to KK elements, i.e. O(K).
+        During the backtracking, we used a list to keep the current combination, which holds up to K elements, i.e. O(K).
 
 */
 class Solution {
